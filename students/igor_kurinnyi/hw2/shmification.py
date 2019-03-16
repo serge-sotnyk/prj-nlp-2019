50d4dc96ca68f917d5cdb6765671495c20eccfcf
Igor Kurinnyi <igorkurinniu@gmail.com>
2019-03-16 13:28:29 +0200

    shication for phrases

--- students/igor_kurinnyi/hw2/shmification.py
+++ students/igor_kurinnyi/hw2/shmification.py
@@ -1,3 +1,4 @@
+import re
 
 class EngLang:
 	sh  = 'sh'
@@ -22,31 +23,63 @@ class Shmificator:
 	def __call__(self, word):
 		return self.shmificate(word)
 
-	def shmificate(self, word):
-		if not word:
+
+	def shmificate(self, phrase):
+		if not phrase:
 			return ''
 
+		*pre_words, word = phrase.split(' ')
+		shmord = self.shmificate_word(word)
+		shmase = self.make_shmase(pre_words, shmord)
+		return '{}-{}'.format(phrase, shmase)
+
+
+	def shmificate_word(self, word):
+		shm = self.choose_shm(word)
+		shm, word = self.case_aligment(shm, word)
+		shmord = self.make_shmord(shm, word)
+		return shmord
+
+
+	def choose_shm(self, word):
 		if self.lang.sh in word:
 			shm = self.lang.sm
 		else:
 			shm = self.lang.shm
+		return shm
 
+
+	def case_aligment(self, shm, word):
 		if word.title() == word:
 			shm = shm.title()
+			word = word.lower()
+		elif word.upper() == word:
+			shm = shm.upper()
+		return shm, word
 
+
+	def make_shmord(self, shm, word):
 		if word[:len(self.lang.shm)] == self.lang.shm:
 			shmord = word
 		else:
 			shmord = shm + self.cut_first_consonants(word)
-
-		return '{}-{}'.format(word, shmord)
+		return shmord
 
 
 	def cut_first_consonants(self, word):
-	    pattern = re.compile(r'.*?(?=[{vowels}])'.format(vowels=EngLang.vowels))
+	    pattern = re.compile(r'.*?(?=[{vowels}])'.format(vowels=self.lang.vowels))
 	    _, end = re.match(pattern,word).span()
 	    return word[end:]
-	    
+
+
+	def make_shmase(self, pre_words, shmord):
+		if pre_words:
+			shmase = '{} {}'.format(' '.join(pre_words), shmord)
+		else:
+			shmase = shmord
+		return shmase
+
+
 
 def test_eng():
 	shm = Shmificator(lang=EngLang)
@@ -56,7 +89,9 @@ def test_eng():
 	assert shm('apple')     == 'apple-shmapple'
 	assert shm('shmutter')  == 'shmutter-shmutter'
 	assert shm('ashmont')   == 'ashmont-smashmont'
-	assert shm('Apple')     == 'Apple-Shmaple' 
+	assert shm('Apple')     == 'Apple-Shmapple' 
+	assert shm('Jimmy Hendriks') == 'Jimmy Hendriks-Jimmy Shmendriks'
+	assert shm('data science') == 'data science-data shmience'
 
 	print('Tests for Eng passed')
 
