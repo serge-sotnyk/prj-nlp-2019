364a7eb23162b15e30f5bf9d1190fdb17c5024d4
serge-sotnyk <sergesotnyk@gmail.com>
2019-03-29 13:30:52 +0200

    04/FactChecking, draft with f1 counting

--- students/SergeSotnyk/04-nlp-full-circle/FactChecking/paul_works_retriever.py
+++ students/SergeSotnyk/04-nlp-full-circle/FactChecking/paul_works_retriever.py
@@ -1,11 +1,20 @@
+from collections import defaultdict
 from itertools import zip_longest
-from typing import List, Optional
+from typing import List, Optional, Dict
 
+import spacy
 import wikipedia
 import os
 
 _start_tag = '<*'
 _finish_tag = '*>'
+print('Start downloading spacy')
+_nlp = spacy.load("en_core_web_md")
+print('Spacy has been downloaded')
+
+
+class _NotThisSuspect(Exception):
+    ...
 
 
 class Entry:
@@ -18,6 +27,86 @@ class Annotation:
     def __init__(self, text: str = '', entries: List[Entry] = None):
         self.text = text
         self.entries = entries if entries else []
+
+
+class Checker:
+    def __init__(self, tsv_name: str):
+        self._lemma_dict = defaultdict(list)
+        self._first_lemmas_dict = defaultdict(list)
+        for rec in self.load_writings_tsv(tsv_name):
+            doc = _nlp(rec)
+            lemmas = [w.lemma_.casefold() for w in doc]
+            self._first_lemmas_dict[lemmas[0]].append(lemmas)
+            for l in lemmas:
+                self._lemma_dict[l].append(lemmas)
+
+    def max_len(self):
+        mx = 0
+        max_lemmas_set = []
+        for lemmas_list_set in self._first_lemmas_dict.values():
+            for lemmas in lemmas_list_set:
+                l = len(lemmas)
+                if l > mx:
+                    mx = l
+                    max_lemmas_set = lemmas
+        print(max_lemmas_set)
+        # res = max(len(lemmas) for lemmas in [lemmas_list_set for lemmas_list_set in self._first_lemmas_dict])
+        return mx
+
+    def _check1(self, suspect_lemmas: List[str]) -> bool:
+        for l in suspect_lemmas:  # check if all suspect's lemmas is in the vocabulary
+            if l not in self._lemma_dict:
+                return False
+        for cluster in self._lemma_dict[suspect_lemmas[0]]:  # check a lemmas order and size
+            if len(suspect_lemmas) != len(cluster):
+                continue
+            for a, b in zip_longest(suspect_lemmas, cluster):
+                if a != b:
+                    continue
+            return True
+        return False
+
+    def check_with_order(self, suspect_lemmas: List[str]) -> bool:
+        if suspect_lemmas[0] not in self._first_lemmas_dict:
+            return False
+        if lemmas_starts_with(suspect_lemmas, ['call', 'me', 'joe']):
+            ...
+        for lemmas in self._first_lemmas_dict[suspect_lemmas[0]]:
+            try:
+                if len(lemmas) != len(suspect_lemmas):
+                    raise _NotThisSuspect()
+                for a, b in zip(lemmas, suspect_lemmas):
+                    if a != b:
+                        raise _NotThisSuspect()
+                return True
+            except _NotThisSuspect:
+                continue
+        return False
+
+    def check(self, suspect: str) -> bool:
+        doc = _nlp(suspect)
+        lemmas = [w.lemma_.casefold() for w in doc]
+        return self._check1(lemmas)
+
+    @staticmethod
+    def load_writings_tsv(tsv_name: str) -> List[str]:
+        res = []
+        with open(tsv_name, 'rt', encoding='utf-8') as f:
+            next(f)  # skip titles
+            for line in f:
+                name, collection = line.split('\t')
+                name = name.strip().replace('"', '')
+                name = name.split('(')[0]  # strip explanations in ()
+                res.append(name)
+        return res
+
+
+def lemmas_starts_with(lemmas: List[str], pattern: List[str])->bool:
+    pattern_len = len(pattern)
+    for a, b in zip_longest(lemmas[:pattern_len], pattern):
+        if a!=b:
+            return False
+    return True
 
 
 def load_annotation(entity: str) -> Optional[Annotation]:
@@ -83,22 +172,87 @@ def show_difference(content, annotation):
             return
 
 
+def find_writings2(content: str, checker: Checker) -> Annotation:
+    res = Annotation(content)
+    doc = _nlp(content)
+    for ne in doc.ents:
+        if checker.check(str(ne)):
+            res.entries.append(Entry(ne.start_char, ne.end_char))
+    return res
+
+
+def find_writings(content: str, checker: Checker) -> Annotation:
+    res = Annotation(content)
+    doc = _nlp(content)
+    max_len = checker.max_len()
+    for sent in doc.sents:
+        sent_lemmas = [w.lemma_.casefold() for w in sent]
+        sent_len = len(sent_lemmas)
+        for p in range(0, sent_len):
+            for l in range(1, max_len + 1):
+                finish_token = p + l
+                if finish_token > sent_len:
+                    break
+                suspect = sent_lemmas[p:finish_token]
+                if checker.check_with_order(suspect):
+                    start = sent[p].idx
+                    finish = sent[finish_token-1].idx + len(sent[finish_token-1])
+                    res.entries.append(Entry(start, finish))
+    return res
+
+
+def compare_with_ground_truth(gt: Annotation, checked: Annotation) -> Dict:
+    res = {}
+    total_in_gt = len(gt.entries)
+    total_in_checked = len(checked.entries)
+    gt_entries_set = set(str(e.start) + '_' + str(e.finish) for e in gt.entries)
+    checked_entries_set = set(str(e.start) + '_' + str(e.finish) for e in checked.entries)
+    tp = len(gt_entries_set.intersection(checked_entries_set))
+    fp = len(checked_entries_set) - tp
+    fn = len(gt_entries_set) - tp
+    precision = tp / (tp + fp)
+    recall = tp / (tp + fn)
+    f1 = 2 * precision * recall / (precision + recall)
+    return {
+        'total_in_gt': total_in_gt,
+        'total_in_checked': total_in_checked,
+        'tp': tp,
+        'fp': fp,
+        'fn': fn,
+        'precision': precision,
+        'recall': recall,
+        'f1': f1,
+    }
+
+
 def main():
     entity = 'Poul Anderson'
     content: str = retrieve_wiki_page(entity)
     if not content:
         return
 
-    annotation = load_annotation(entity)
-    if not annotation:
+    manual_annotation = load_annotation(entity)
+    if not manual_annotation:
         return
-    if content != annotation.text:
+    if content != manual_annotation.text:
         print('Annotation content is differ from Wikipedia text.')
-        show_difference(content, annotation)
+        show_difference(content, manual_annotation)
         return
-    print(f"Annotated entries number: {len(annotation.entries)}")
-    for e in annotation.entries:
+    print(f"Annotated entries number: {len(manual_annotation.entries)}")
+    for e in manual_annotation.entries:
         print(content[e.start:e.finish])
+
+    print('-' * 64)
+
+    checker = Checker('sparql.tsv')
+    auto_annotation = find_writings(content, checker)
+    print(f"Found entries number: {len(auto_annotation.entries)}")
+    for e in auto_annotation.entries:
+        print(content[e.start:e.finish])
+
+    print('=' * 64)
+    for name, value in compare_with_ground_truth(manual_annotation, auto_annotation).items():
+        print(f"{name}: {value}")
 
 
 if __name__ == '__main__':
