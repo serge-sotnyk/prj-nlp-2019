8bafa6849e306d6637cf748f1de76677dba5cdb3
Eugene Kostrov <shtirlitss@gmail.com>
2019-04-07 17:50:53 +0300

    additional dataset analyzed

--- students/kostrov/05-bag-of-words/02-classification/prepare_data.py
+++ students/kostrov/05-bag-of-words/02-classification/prepare_data.py
@@ -13,17 +13,15 @@ def filter_data(data):
 def prepare_data(dataframe):
     df = dataframe.copy()
     df['review'] = df['review'].str.lower()
-    df['pros'] = df['pros'].str.lower()
-    df['cons'] = df['cons'].str.lower()
+    df['pros'] = df['pros'].str.lower().str.replace(r'[()+\-*=\/\\]+', ' ').str.replace(r'\d+', ' ').str.replace(r'\s+', ' ').str.strip()
+    df['cons'] = df['cons'].str.lower().str.replace(r'[()+\-*=\/\\]+', ' ').str.replace(r'\d+', ' ').str.replace(r'\s+', ' ').str.strip()
     df = df[df['review'].str.contains('[А-ЯҐЄІЇа-яґєії]')]
     df = df[df[['pros', 'cons']].apply(filter_data, axis=1)]
-    df['pros'] = df['pros'].str.replace(r'[()+\-*=\/\\]+', ' ').str.replace(r'\d+', ' ').str.replace(r'\s+', ' ').str.strip()
-    df['cons'] = df['cons'].str.replace(r'[()+\-*=\/\\]+', ' ').str.replace(r'\d+', ' ').str.replace(r'\s+', ' ').str.strip()
     df['review'] = df['review'].str.replace(r'[()+\-*=\/\\]+', ' ').str.replace(r'\d+', ' ').str.replace(r'\s+', ' ').str.strip()
     df = df[df['review'].apply(lambda x: detect(x) == 'uk')]
     return df
 
 if __name__ == '__main__':
-    df = pd.read_csv('../01-crawling/data/output/data.tsv', sep='\t', index_col=0, names=['id', 'rating', 'item_bought', 'review', 'pros', 'cons', 'upvotes', 'downvotes'])
+    df = pd.read_csv('../01-crawling/data/output/data2.tsv', sep='\t', index_col=0, names=['id', 'rating', 'item_bought', 'review', 'pros', 'cons', 'upvotes', 'downvotes'])
     result = prepare_data(df)
-    result.to_csv('data/input/comments.tsv', sep='\t', quoting=csv.QUOTE_NONE)
+    result.to_csv('data/input/comments2.tsv', sep='\t', quoting=csv.QUOTE_NONE)
